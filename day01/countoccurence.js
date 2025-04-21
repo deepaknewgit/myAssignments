5e63f2b2a64c85b528406971d971134019808209
deepaknewgit <deepak14ec09@gmail.com>
2025-04-21 15:15:20 +0530

    feat: add new file

---
+++ day01/countoccurence.js
@@ -0,0 +1,17 @@
+let orginalword = "Deepaksarath"
+let count = 0
+
+for(let i=0;i<=orginalword.length;i++)
+{
+
+    if(orginalword.charAt(count)==orginalword.charAt(i))
+    {
+    
+     console.log(orginalword.charAt(i)+":"+count++)
+     
+
+    }
+
+
+}
+
